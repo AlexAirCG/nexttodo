8b9e6bea2f95b2dbfada47cd7815d69c8d1ae146
AlexAirCG <alexairggg@yandex.ru>
2025-02-26 20:02:54 +0300

    26.02.25

---
+++ src/app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,28 @@
+import NextAuth from "next-auth"
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaClient } from "@prisma/client"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+
+const prisma = new PrismaClient()
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+
+    // ...add more providers here
+  ],
+}
+
+const handler = NextAuth(authOptions);
+
+export {handler as GET, handler as POST};
